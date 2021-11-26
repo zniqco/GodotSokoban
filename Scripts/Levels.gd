ff36a113ad7742b7c41a7ca0d8bc550e2c62e0eb
zniq <50171985+zniqco@users.noreply.github.com>
2021-11-27 04:01:42 +0900

    Runnable but dirty codes

--- Scripts/Levels.gd
+++ Scripts/Levels.gd
@@ -1,7 +1,40 @@
 extends Node
 
-func _ready():
-	print("Ready")
+enum {
+	NONE,
+	LEVEL,
+}
+
+var levels = []
+
+func load_levels(filename: String):
+	var file = File.new()
+	var state = NONE
+	var lines = []
 	
-func test():
-	print("Test")
+	levels.clear()
+	file.open(filename, File.READ)
+	
+	while !file.eof_reached():
+		var line = file.get_line()
+		var first_character = ""
+		
+		if line.length() >= 1:
+			first_character = line.left(1)
+
+		if state == NONE:
+			if first_character != "" and first_character != ";":
+				state = LEVEL
+					
+		if state == LEVEL:
+			if first_character == ";":
+				levels.append(lines)
+				lines = []
+				state = NONE
+			else:
+				lines.append(line)
+	
+	file.close()
+
+func get_level(index: int):
+	return levels[index]
